d76b8117ad4383684827606b00b964b8a59591ab
lucas <lucasluan94@hotmail.com>
2023-03-04 22:09:38 -0300

    First commit

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,13 +1,15 @@
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
+import Navbar from '@/components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main>
-      <div className=''>
+      <div>
+        <Navbar />
         <h1>oi</h1>
       </div>
     </main>
---
+++ app/src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+export default function Navbar(){
+    return(
+        <div className="Navbar">
+            <div className="logo">
+                <h1>StarCafe</h1>
+            </div>
+            <p>Essa é a navbar</p>
+        </div>
+    )
+}
