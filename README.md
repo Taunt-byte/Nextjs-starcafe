ec0f2395c5161e0a32fb42aba6a08bc448c0f4dc
lucas <lucasluan94@hotmail.com>
2023-05-08 14:31:03 -0300

    ADD Navbar

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Starbucks-UI
+# Startcafe
+
+Aplicação web de uma cafeteria em Nextjs com foco na venda de refil de cafeteiras e cafeteiras.
--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -1,10 +1,16 @@
+import Link from "next/link";
+
 export default function Navbar(){
     return(
-        <div className="Navbar">
-            <div className="logo">
+        <div className="flex">
+        <nav className="fixed top-0 z-50 w-full bg-gray-800 dark:bg-gray-900 shadow-lg transition-all py-4 px-6">
+            <div className="text-bold text-amber-500 font-xl">
                 <h1>StarCafe</h1>
             </div>
-            <p>Essa é a navbar</p>
+            <div>
+                <Link href={"/"}>Inicio</Link>
+            </div>
+        </nav>
         </div>
     )
 }
